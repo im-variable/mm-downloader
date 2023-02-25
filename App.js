af0de4a5ea6813dfec59392ec2df27a762c84f0b
sandeep-hannovit <sandeep.negi@hannovit.com>
2023-02-25 17:34:11 +0530

    initial commit for project setup

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { ToastAndroid } from "react-native";
 import {
   Text,
+  Input,
+  View,
   Link,
   HStack,
   Center,
@@ -10,10 +13,19 @@ import {
   NativeBaseProvider,
   extendTheme,
   VStack,
+  Stack,
   Box,
+  Image,
+  Button,
+  Progress,
 } from "native-base";
+
+import ytdl from "react-native-ytdl";
+import * as FileSystem from "expo-file-system";
+
 import NativeBaseIcon from "./components/NativeBaseIcon";
 import { Platform } from "react-native";
+import icon from "./assets/mm.png";
 
 // Define the config
 const config = {
@@ -25,6 +37,80 @@ const config = {
 export const theme = extendTheme({ config });
 
 export default function App() {
+  const [downloadProgress, setDownloadProgress] = useState(0);
+  const [isDownloading, setIsDownloading] = useState(false);
+  const [url, setUrl] = useState("");
+
+  const handleChangeUrl = (text) => setUrl(text);
+
+  const downloadBtnHandler = async (e) => {
+    if (url) {
+      try {
+        const videoInfo = await ytdl.getInfo(url);
+        const videoTitle = videoInfo.videoDetails.title + "[ViBE]";
+        const format = ytdl.chooseFormat(videoInfo.formats, {
+          quality: "highestaudio",
+        });
+
+        const permissions =
+          await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
+        if (!permissions.granted) {
+          return;
+        }
+        console.log(permissions.directoryUri);
+
+        // const downloadDir = FileSystem.documentDirectory + "ViBE/";
+        const downloadDir = `${permissions.directoryUri}/`;
+        const fileUri = downloadDir + videoTitle + ".mp3";
+
+        const directoryInfo = await FileSystem.getInfoAsync(downloadDir);
+        if (!directoryInfo.exists) {
+          await FileSystem.makeDirectoryAsync(downloadDir, {
+            intermediates: true,
+          });
+        }
+        // Use the provided URI to get the DocumentFile
+        const documentFile =
+          await FileSystem.StorageAccessFramework.getDocumentAsync(downloadDir);
+        setIsDownloading(true);
+        const file = await FileSystem.createDownloadResumable(
+          format.url,
+          fileUri,
+          {},
+          (downloadprocess) => {
+            const progress = Math.round(
+              (downloadprocess.totalBytesWritten /
+                downloadprocess.totalBytesExpectedToWrite) *
+                100
+            );
+            setDownloadProgress(progress);
+            console.log(`Download progress: ${progress}`);
+          }
+        );
+
+        const downloadResult = await file.downloadAsync();
+
+        await FileSystem.writeAsStringAsync(fileUri, content);
+        setIsDownloading(false);
+        ToastAndroid.showWithGravity(
+          `"${downloadResult.uri}" has been downloaded.`,
+          ToastAndroid.LONG,
+          ToastAndroid.BOTTOM
+        );
+        console.log(`Audio saved to ${downloadResult.uri}`);
+      } catch (error) {
+        ToastAndroid.showWithGravity(
+          "" + error,
+          ToastAndroid.LONG,
+          ToastAndroid.BOTTOM
+        );
+        console.error(error);
+      } finally {
+        setDownloadProgress(0);
+        setIsDownloading(false);
+      }
+    }
+  };
   return (
     <NativeBaseProvider>
       <Center
@@ -34,32 +120,64 @@ export default function App() {
         flex={1}
       >
         <VStack space={5} alignItems="center">
-          <NativeBaseIcon />
-          <Heading size="lg">Welcome to NativeBase</Heading>
-          <HStack space={2} alignItems="center">
-            <Text>Edit</Text>
-            <Box
-              _web={{
-                _text: {
-                  fontFamily: "monospace",
-                  fontSize: "sm",
-                },
-              }}
-              px={2}
-              py={1}
-              _dark={{ bg: "blueGray.800" }}
-              _light={{ bg: "blueGray.200" }}
+          {/* <NativeBaseIcon /> */}
+          <Heading size="lg">
+            <VStack
+              space={1}
+              justifyContent="center"
+              alignItems="center"
+              safeAreaTop // my={6}
+              mb={2}
             >
-              App.js
-            </Box>
-            <Text>and save to reload.</Text>
+              <Image
+                borderRadius={10}
+                key={"xs"}
+                size={50}
+                resizeMode="cover"
+                source={icon}
+                alt={"logo"}
+                accessibilityLabel={"logo"}
+              />
+            </VStack>
+          </Heading>
+          <HStack space={2} alignItems="center">
+            <Input
+              variant="underlined"
+              placeholder="https://www.youtube.com/watch?v=xxxxxxxxx"
+              value={url}
+              onChangeText={handleChangeUrl}
+              disabled={isDownloading}
+            />
           </HStack>
-          <Link href="https://docs.nativebase.io" isExternal>
-            <Text color="primary.500" underline fontSize={"xl"}>
-              Learn NativeBase
-            </Text>
-          </Link>
+          <HStack space={2} alignItems="center">
+            {isDownloading && (
+              <Center w="100%">
+                <Box w="100%" maxW="400">
+                  <Progress value={downloadProgress} mx="4" />
+                </Box>
+                <Text>{downloadProgress}%</Text>
+              </Center>
+            )}
+          </HStack>
+          <Button
+            size="sm"
+            variant="outline"
+            colorScheme="secondary"
+            onPress={downloadBtnHandler}
+            disabled={isDownloading}
+            isLoading={isDownloading}
+            isLoadingText="Downloading"
+          >
+            Download
+          </Button>
           <ToggleDarkMode />
+          <VStack
+            space={1}
+            justifyContent="center"
+            alignItems="center"
+            safeAreaTop // my={6}
+            mb={2}
+          ></VStack>
         </VStack>
       </Center>
     </NativeBaseProvider>
